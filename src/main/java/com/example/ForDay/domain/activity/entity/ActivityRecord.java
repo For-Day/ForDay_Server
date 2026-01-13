ffc5edd9e8a495e92d11249a88a4a2b7773ac64c
Jina Ham <hamjina61@gmail.com>
2026-01-13 14:43:45 +0900

    [#46] feat: Activity 관련 엔티티 정의

---
+++ src/main/java/com/example/ForDay/domain/activity/entity/ActivityRecord.java
@@ -0,0 +1,41 @@
+package com.example.ForDay.domain.activity.entity;
+
+import com.example.ForDay.domain.activity.type.RecordVisibility;
+import com.example.ForDay.domain.user.entity.User;
+import com.example.ForDay.global.common.mapped.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "activity_records")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActivityRecord extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "activity_record_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_activity_id", nullable = false)
+    private Activity activity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, length = 20)
+    private String sticker;
+
+    @Column(length = 100)
+    private String memo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private RecordVisibility visibility;
+}
