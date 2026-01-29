1b06af0402756b05c2b20aa69aff465d0bde1262
Jina Ham <hamjina61@gmail.com>
2026-01-29 18:25:23 +0900

    [#103] test: 친구 끊기 관련 테스트 코드 작성

--- src/test/java/com/example/ForDay/domain/friend/service/FriendServiceTest.java
+++ src/test/java/com/example/ForDay/domain/friend/service/FriendServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.ForDay.domain.friend.service;
 
 import com.example.ForDay.domain.friend.dto.request.AddFriendReqDto;
+import com.example.ForDay.domain.friend.dto.request.BlockFriendReqDto;
+import com.example.ForDay.domain.friend.dto.response.GetFriendListResDto;
 import com.example.ForDay.domain.friend.entity.FriendRelation;
 import com.example.ForDay.domain.friend.repository.FriendRelationRepository;
 import com.example.ForDay.domain.friend.type.FriendRelationStatus;
@@ -11,17 +13,13 @@ import com.example.ForDay.domain.user.type.SocialType;
 import com.example.ForDay.global.common.error.exception.CustomException;
 import com.example.ForDay.global.common.error.exception.ErrorCode;
 import com.example.ForDay.global.oauth.CustomUserDetails;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -101,7 +99,6 @@ class FriendServiceTest {
         friendRelationRepository.save(relation);
 
         // when (A -> B에 친구 맺기 요청)
-        // then (예외 나옴)
         CustomUserDetails userDetails = new CustomUserDetails(userA);
         AddFriendReqDto reqDto = new AddFriendReqDto(userB.getId());
 
@@ -134,10 +131,126 @@ class FriendServiceTest {
                 .socialId("social_b")
                 .build();
         userRepository.save(userB);
-        // B -> A 차단
+        // A -> B 차단
+        FriendRelation relation = FriendRelation.builder()
+                .requester(userB)
+                .targetUser(userA)
+                .relationStatus(FriendRelationStatus.BLOCK)
+                .build();
+        friendRelationRepository.save(relation);
+
+        // when (A -> B에 친구 맺기 요청)
+        CustomUserDetails userDetails = new CustomUserDetails(userA);
+        AddFriendReqDto reqDto = new AddFriendReqDto(userB.getId());
+
+        // then (예외 나옴)
+        // Service에서 CustomException(USER_NOT_FOUND)을 던지는지 검증
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            friendService.addFriend(reqDto, userDetails);
+        });
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 친구끊기_타겟유저가_탈퇴한_회원인경우() {
+        // given
+        // 1. 요청자(A) 생성 및 저장
+        User userA = User.builder()
+                .nickname("요청자A")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_a")
+                .deleted(false)
+                .build();
+        userRepository.save(userA);
+
+        // 2. 탈퇴한 피요청자(B) 생성 및 저장
+        User userB = User.builder()
+                .nickname("요청자B")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_b")
+                .deleted(true)
+                .build();
+        userRepository.save(userB);
+
+        // when
+        CustomUserDetails userDetails = new CustomUserDetails(userA);
+
+        // then (예외 나옴)
+        // Service에서 CustomException(USER_NOT_FOUND)을 던지는지 검증
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            friendService.deleteFriend(userB.getId(), userDetails);
+        });
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 친구끊기_요청자가_타겟유저를_차단중인경우() {
+        // given
+        // 1. 요청자(A) 생성 및 저장
+        User userA = User.builder()
+                .nickname("요청자A")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_a")
+                .build();
+        userRepository.save(userA);
+
+        // 2. 탈퇴한 피요청자(B) 생성 및 저장
+        User userB = User.builder()
+                .nickname("요청자B")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_b")
+                .build();
+        userRepository.save(userB);
+        // A -> B 차단
         FriendRelation relation = FriendRelation.builder()
                 .requester(userA)
                 .targetUser(userB)
+                .relationStatus(FriendRelationStatus.BLOCK)
+                .build();
+        friendRelationRepository.save(relation);
+
+        // when
+        CustomUserDetails userDetails = new CustomUserDetails(userA);
+
+        // then (예외 나옴)
+        // Service에서 CustomException(USER_NOT_FOUND)을 던지는지 검증
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            friendService.deleteFriend(userB.getId(), userDetails);
+        });
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 친구끊기_타겟유저가_요청자를_차단중인경우() {
+        // given
+        // 1. 요청자(A) 생성 및 저장
+        User userA = User.builder()
+                .nickname("요청자A")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_a")
+                .build();
+        userRepository.save(userA);
+
+        // 2. 탈퇴한 피요청자(B) 생성 및 저장
+        User userB = User.builder()
+                .nickname("요청자B")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_b")
+                .build();
+        userRepository.save(userB);
+        // B -> A 차단
+        FriendRelation relation = FriendRelation.builder()
+                .requester(userB)
+                .targetUser(userA)
                 .relationStatus(FriendRelationStatus.BLOCK)
                 .build();
         friendRelationRepository.save(relation);
@@ -155,5 +268,84 @@ class FriendServiceTest {
 
         assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
     }
+
+    @Test
+    void 친구차단_타겟유저가_탈퇴한_회원인경우() {
+        // given
+        // 1. 요청자(A) 생성 및 저장
+        User userA = User.builder()
+                .nickname("요청자A")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_a")
+                .deleted(false)
+                .build();
+        userRepository.save(userA);
+
+        // 2. 탈퇴한 피요청자(B) 생성 및 저장
+        User userB = User.builder()
+                .nickname("요청자B")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_b")
+                .deleted(true)
+                .build();
+        userRepository.save(userB);
+
+        // when
+        CustomUserDetails userDetails = new CustomUserDetails(userA);
+        BlockFriendReqDto reqDto = new BlockFriendReqDto(userB.getId());
+
+        // then (예외 나옴)
+        // Service에서 CustomException(USER_NOT_FOUND)을 던지는지 검증
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            friendService.blockFriend(reqDto, userDetails);
+        });
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @Test
+    void 친구차단_타겟유저가_요청유저를_차단한경우() {
+        // given
+        // 1. 요청자(A) 생성 및 저장
+        User userA = User.builder()
+                .nickname("요청자A")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_a")
+                .build();
+        userRepository.save(userA);
+
+        // 2. 탈퇴한 피요청자(B) 생성 및 저장
+        User userB = User.builder()
+                .nickname("요청자B")
+                .role(Role.USER)
+                .socialType(SocialType.KAKAO)
+                .socialId("social_b")
+                .build();
+        userRepository.save(userB);
+
+        // B -> A 차단
+        FriendRelation relation = FriendRelation.builder()
+                .requester(userB)
+                .targetUser(userA)
+                .relationStatus(FriendRelationStatus.BLOCK)
+                .build();
+        friendRelationRepository.save(relation);
+
+        // when
+        CustomUserDetails userDetails = new CustomUserDetails(userA);
+        BlockFriendReqDto reqDto = new BlockFriendReqDto(userB.getId());
+
+        // then (예외 나옴)
+        // Service에서 CustomException(USER_NOT_FOUND)을 던지는지 검증
+        CustomException exception = assertThrows(CustomException.class, () -> {
+            friendService.blockFriend(reqDto, userDetails);
+        });
+
+        assertEquals(ErrorCode.USER_NOT_FOUND, exception.getErrorCode());
+    }
+
 
 }
