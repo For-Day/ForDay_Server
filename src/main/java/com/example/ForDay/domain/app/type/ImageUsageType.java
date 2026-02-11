187406a3bbda0dc62cf53e155f5fa285a3ad203d
Jina Ham <hamjina61@gmail.com>
2026-02-11 15:46:20 +0900

    fix: presigned url 발급시 test용 enum값 추가

--- src/main/java/com/example/ForDay/domain/app/type/ImageUsageType.java
+++ src/main/java/com/example/ForDay/domain/app/type/ImageUsageType.java
@@ -3,5 +3,9 @@ package com.example.ForDay.domain.app.type;
 public enum ImageUsageType {
     ACTIVITY_RECORD,
     PROFILE_IMAGE,
-    COVER_IMAGE
+    COVER_IMAGE,
+
+    TEST_ACTIVITY_RECORD,
+    TEST_PROFILE_IMAGE,
+    TEST_COVER_IMAGE
 }
